7a98dc048171aff247463b7c44ebb2095fffb0f7
heimsharon <heimsharon@hotmail.com>
2025-04-12 03:50:47 -0400

    more config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env files
+dotenv.config();
+
+const port = parseInt(process.env.PORT || '5173', 10);
 
 // https://vitejs.dev/config/
 export default defineConfig({
- 
   plugins: [react()],
+  preview: {
+    host: true,
+    port, // Use the dynamically loaded port
+    allowedHosts: ['candidate-search-pdrr.onrender.com'], // Replace with your Render domain
+  },
 });
